6caf88872b9a81c0eb8549ca36a347fc0a92382b
Sujan Baskota <sujan.baskota77@gmail.com>
2024-05-09 11:33:51 +0545

    fixed lowercase days

--- commands/utilities/routine.js
+++ commands/utilities/routine.js
@@ -8,30 +8,32 @@ const dayMap = {
   wednesday: 3,
   thuursday: 4,
   friday: 5,
-  saturday: 6
-}
-console.log(dayMap['tue'])
+  saturday: 6,
+};
+console.log(dayMap["tue"]);
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("routine")
     .setDescription("get today's routine")
-    .addStringOption((option) => option.setName("day").setDescription("which day")),
+    .addStringOption((option) =>
+      option.setName("day").setDescription("which day")
+    ),
   async execute(interaction) {
-    const day =
-      dayMap[interaction.options.getString("day")] === 0
-        ? 0
-        : dayMap[interaction.options.getString("day")] || new Date().getDay();
-    if (day=== 6) await interaction.reply("# Happy Saturday");
+    let day = new Date().getDay();
+    if (interaction.options?.getString("day"))
+      day = dayMap[interaction.options?.getString("day")?.toLowerCase()];
+    if (day === 6) await interaction.reply("# Happy Saturday");
     else {
       const today = routine[Object.keys(routine)[day]];
       let string = "";
+      string = string + Object.keys(dayMap)[day].toUpperCase() + "\n";
       today.forEach((el) => {
         el.group === "both"
           ? (string += `${el.subject} - ${el.instructor} [${el.from} - ${el.to}]\n`)
           : (string += `${el.group}: ${el.subject} - ${el.instructor} [${el.from} - ${el.to}]\n`);
       });
-      await interaction.reply("```" + string + "```");
+      await interaction.reply("```" + "\n" + string + "```");
     }
   },
 };
---
+++ temp.js
@@ -0,0 +1,12 @@
+const dayMap = {
+  sunday: 0,
+  monday: 1,
+  tuesday: 2,
+  wednesday: 3,
+  thuursday: 4,
+  friday: 5,
+  saturday: 6,
+};
+
+
+console.log(Object.keys(dayMap)[0])
